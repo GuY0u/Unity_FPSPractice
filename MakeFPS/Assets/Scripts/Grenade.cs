0231a34a08290baf6b67ba37fa593fa2b16991d5
GuYou <yysh123@gmail.com>
2020-06-16 16:16:15 +0900

    플레이어 총알 폭탄 생성
    - 플레이어 레이캐스트, 레이히트를 이용한 총알 발사
    - rigidbody, addforce를 이용한 포물선 폭탄던지기
    - 둘다 총알은 끝지점, 폭탄은 충돌지점에 이펙트 발생

---
+++ MakeFPS/Assets/Scripts/Grenade.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+
+    //폭탄의 역할
+    //예전 총알은 생성하면 스스로 날라가다 충돌하면 터졌다
+    //하지만 폭탄은 생성되자마자 스스로 이동하면 될까 안될까?
+    //폭탄은 플레이어가 직접던져야된다
+    //폭탄이 다른 오브젝트들과 충돌하면 터진다
+
+    public GameObject fxFactory;
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        //폭발이펙트 보여주기
+        GameObject fx = Instantiate(fxFactory);
+        fx.transform.position = transform.position;
+        //혹시나 이펙트오브젝트가 사라지지 않는 경우
+        //Destroy(fx, 2.0f); //2초후에 폭발이펙트 삭제
+
+
+        //다른 오브젝트도 삭제하기
+        //자기자신 삭제하기 (맨 마지막에 처리)
+    }
+    
+}
--- MakeFPS/Assets/Scripts/PlayerMove.cs
+++ MakeFPS/Assets/Scripts/PlayerMove.cs
@@ -6,6 +6,13 @@ public class PlayerMove : MonoBehaviour
 {
     float speed = 5.0f;
     CharacterController cCon;
+    public float jumpPower = 10;
+    public int jumpCountMax = 2;
+    public int jumpCount = 0;
+
+    //중력적용
+    public float gravity = -5;
+    float velocityY;    //낙하속도(벨로시티는 방향과 힘을 들고 있다.)
 
     // Start is called before the first frame update
     void Start()
@@ -40,6 +47,45 @@ public class PlayerMove : MonoBehaviour
         //캐릭터컨트롤러는 충돌감지만 하고 물리가 적용안된다
         //따라서 충돌감지를 하기 위해서는 반드시
         //캐릭터컨트롤러 컴포넌트가 제공해주는 함수로 이동처리해야 한다.
+        //cCon.Move(dir * speed * Time.deltaTime);
+
+        velocityY += gravity * Time.deltaTime;
+        dir.y = velocityY;
         cCon.Move(dir * speed * Time.deltaTime);
+
+        //캐릭터 점프
+        //점프버튼을 누르면 수직속도에 점프파워를 넣는다.
+        //땅에 닿으면 0으로 초기화
+        //if (cCon.isGrounded)
+        //{
+        //    jumpCount = 0;
+        //    velocityY = 0;
+        //}
+        //else
+        //{
+        //    velocityY += gravity * Time.deltaTime;
+        //    dir.y = velocityY;
+        //}
+
+        if (cCon.collisionFlags == CollisionFlags.Above) //땅에 닿았는가?
+        {
+            jumpCount = 0;
+            velocityY = 0;
+        }
+        else
+        {
+            velocityY += gravity * Time.deltaTime;
+            dir.y = velocityY;
+        }
+
+        //점프하는 부분
+        if (Input.GetButtonDown("Jump") && jumpCount < 2)
+        {
+            jumpCount++;
+            velocityY = jumpPower;
+        }
+        //CollisionFlags.Above; -> 위 쪽 충돌
+        //CollisionFlags.Sides; -> 중간 옆쪽 충돌
+        //CollisionFlags.Below; -> 아래 쪽 충돌
     }
 }
